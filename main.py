bc4e049d82893bbc60ece21f8835affc0cba6ea3
luvleenrehal <73304147+luvleenrehal@users.noreply.github.com>
2021-01-09 18:17:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-
+"""author: Luvleen Rehal, Meghna Ghosh"""
 from tkinter import *
 
 root = Tk()
-root.configure(background="lightblue")
+root.configure(background="seagreen")
 
 high_70s = ['75', '76', '77', '78', '79']
 low_80s = ['80', '81', '82', '83', '84', '85']
@@ -11,82 +11,112 @@ low_90s = ['90', '91', '92', '93', '94']
 high_90s = ['95', '96', '97', '98', '99', '100']
 
 greetings = ['hi', 'hello', "Hello", "Hi", "Hey", "hey"]
-a = "Arts/Media, Social Sciences"
+
 
 def send():
     send = "You -> " + e.get()
     txt.insert(END, "\n" + send)
     if e.get() in greetings:
-        txt.insert(END, "\n" + "Bot -> Hello! I am Uniqua. I'm here to "
+        txt.insert(END, "\n" + "UniQua -> Hello! I am UniQua. I'm here to "
                                "help and guide you on which university and "
                                "program you should apply to. To get started, "
-                               "enter 2 fields/programs you are interested in. "
+                               "enter 2 programs you are interested in. "
                                "Choose from these given options: Arts/Media, "
                                "Computer Science, Healthcare, Social Sciences, "
                                "Business, Engineering")
 
     # get programs
+
+    #first option
     elif e.get() == "Arts/Media, Social Sciences":
-        txt.insert(END, "\n" + "Bot -> Wonderful! The universities that align "
+        txt.insert(END, "\n" + "UniQua -> Wonderful! The universities that align "
                                "with your interests are: York, Ryerson, UofT, "
                                "McMaster")
-        txt.insert(END, "\n" + "Bot -> Now I just need your most current "
+        txt.insert(END, "\n" + "UniQua -> Now I just need your most current "
                                "grade 12 average to help you further.")
+    # for arts
+    elif e.get() in high_70s:
+        txt.insert(END, "\n" + "UniQua -> Based on your average and interests, I "
+                               "have determined the best suited program for you!:"
+                               "Arts/Media at York University")
+    # for social Science
+    elif e.get() in low_80s:
+        txt.insert(END, "\n" + "UniQua -> Based on your average and interests, I "
+                               "have determined the best suited program for you!:"
+                               "Social Sciences at Ryerson University")
+
+    # second option
     elif e.get() == "Computer Science, Business":
-        txt.insert(END, "\n" + "Bot -> Wonderful! The universities that align "
+        txt.insert(END, "\n" + "UniQua -> Wonderful! The universities that align "
                                "with your interests are: UofT, Waterloo, "
                                "York, Laurier, Ryerson")
-        txt.insert(END, "\n" + "Bot -> Now I just need your most current "
+        txt.insert(END, "\n" + "UniQua -> Now I just need your most current "
                                "grade 12 average to help you further.")
+    # for business
+    elif e.get() in low_80s:
+        txt.insert(END, "\n" + "UniQua -> Based on your average and interests, I "
+                               "have determined the best suited program for you!:"
+                               "Business at Ryerson")
+    # for computer science
+    elif e.get() in low_90s:
+        txt.insert(END, "\n" + "UniQua -> Based on your average and interests, I "
+                               "have determined the best suited program for you!:"
+                               "Computer Science at Waterloo")
+
+
+    # third option
     elif e.get() == "Healthcare, Business":
-        txt.insert(END, "\n" + "Bot -> Wonderful! The universities that align "
+        txt.insert(END, "\n" + "UniQua -> Wonderful! The universities that align "
                                "with your interests are: McMaster, York, "
                                "Ryerson, Western")
-        txt.insert(END, "\n" + "Bot -> Now I just need your most current "
+        txt.insert(END, "\n" + "UniQua -> Now I just need your most current "
                                "grade 12 average to help you further.")
+    # for healthcare
+    elif e.get() in low_90s:
+        txt.insert(END, "\n" + "UniQua -> Based on your average and interests, I "
+                               "have determined the best suited program for you!:"
+                               "Health sciences at McMaster University")
+    # for Business
+    elif e.get() in high_80s:
+        txt.insert(END, "\n" + "UniQua -> Based on your average and interests, I "
+                               "have determined the best suited program for you!:"
+                               "Business at York University")
+
+    # fourth option
     elif e.get() == "Arts/Media, Engineering":
-        txt.insert(END, "\n" + "Bot -> Wonderful! The universities that align "
+        txt.insert(END, "\n" + "UniQua -> Wonderful! The universities that align "
                                "with your interests are: York, Ryerson"
                                "Waterloo, Queens")
-        txt.insert(END, "\n" + "Bot -> Now I just need your most current "
+        txt.insert(END, "\n" + "UniQua -> Now I just need your most current "
                                "grade 12 average to help you further.")
-
-    # get averages
-    elif e.get() in high_70s:
-        txt.insert(END, "\n" + "Bot -> Based on your average and interests, I "
-                               "have determined the best suited program for you is at York")
-    elif e.get() in low_80s:
-        txt.insert(END, "\n" + "Bot -> Based on your average and interests, I "
-                               "have determined the best suited program for you is at Laurier!")
-    elif e.get() in high_80s:
-        txt.insert(END, "\n" + "Bot -> Based on your average and interests, I "
-                               "have determined the best suited program for you is at Waterloo!")
-
-    elif e.get() in low_90s:
-        txt.insert(END, "\n" + "Bot -> Based on your average and interests, I "
-                               "have determined the best suited program for you is at UofT!")
+    # for engineering
     elif e.get() in high_90s:
-        txt.insert(END, "\n" + "Bot -> Based on your average and interests, I "
-                               "have determined the best suited program for you is at McMaster!")
-
-
+        txt.insert(END, "\n" + "UniQua -> Based on your average and interests, I "
+                               "have determined the best suited program for you!:"
+                               "Engineering at Waterloo")
+    # for Arts
+    elif e.get() in low_80s:
+        txt.insert(END, "\n" + "UniQua -> Based on your average and interests, I "
+                               "have determined the best suited program for you!:"
+                               "Design/Media at Ryerson")
+    # if bot cannot understand
     else:
-        txt.insert(END, "\n" + "Bot -> Sorry I do not understand. Please try "
+        txt.insert(END, "\n" + "UniQua -> Sorry I do not understand. Please try "
                                "typing again.")
     e.delete(0, END)
 
-# GUI (i combined my gui code to make it look cleaner)
+
+# GUI
 root.geometry("1000x500")
 root.title("Virtual Assistant")
 
 txt = Text(root)
-#scrollbar = Scrollbar(txt)
-txt.grid(row=0, column=0, columnspan=2)
+txt.grid(row=0, column=0, columnspan=1)
 
-e = Entry(root, width=80)
-#send = Button(root, text="SEND", command=send).grid(row=1, column=1)
-button = Button(root, bg="Red", fg= "blue", text = "Ask Assistant",
-                font = ("Times New Roman", 18), command=send).grid(row=2, column=2)
+e = Entry(root, width=90)
+button = Button(root, bg="Orange", fg= "White", text = "Ask UniQua",
+                font = ("Trebuchet Ms", 25), command=send).grid(row=2, column=2)
+
 e.grid(row=1, column=0)
-root.title("Chatbot")
+root.title("UniQua: Your Virtual Assistant")
 root.mainloop()
